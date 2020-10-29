e8d5eead345c6ac84506adc5c0b3d65e90b1ffb5
Jenna Pederson <jennamp@amazon.com>
2020-10-28 20:04:40 -0500

    add storage

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
-
-// import API from Amplify library
-import { API } from 'aws-amplify'
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-
-// import query definition
-import { listPosts } from './graphql/queries'
+import { Storage } from 'aws-amplify'
+import { v4 as uuid } from 'uuid'
 
 function App() {
-  const [posts, setPosts] = useState([])
+  const [images, setImages] = useState([])
   useEffect(() => {
-    fetchPosts();
-  }, []);
-  async function fetchPosts() {
-    try {
-      const postData = await API.graphql({ query: listPosts });
-      setPosts(postData.data.listPosts.items)
-    } catch (err) {
-      console.log({ err })
-    }
+    fetchImages()
+  }, [])
+  async function fetchImages() {
+    // Fetch list of images from S3
+    let s3images = await Storage.list('')
+    // Get presigned URL for S3 images to display images in app
+    s3images = await Promise.all(s3images.map(async image => {
+      const signedImage = await Storage.get(image.key)
+      return signedImage
+    }))
+    setImages(s3images)
   }
+  function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    // upload the image then fetch and rerender images
+    Storage.put(uuid(), file).then (() => fetchImages())
+  }
+
   return (
     <div>
       <h1>Postagram</h1>
-      {
-        posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.name}</h3>
-            <p>{post.description}</p>
-          </div>
-        ))
-      }
+      <span>Add new image</span>
+      <input
+        type="file"
+        accept='image/png'
+        onChange={onChange}
+      />
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      { images.map(image => <img src={image} style={{width: 400, marginBottom: 10}} />) }
+      </div>
       <AmplifySignOut />
     </div>
-  )
+  );
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
