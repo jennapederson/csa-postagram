7a039db981610f18dc676a34c4b6af16557fa5b0
Jenna Pederson <jennamp@amazon.com>
2020-10-28 19:29:24 -0500

    add api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
 
-function App() {
+// import API from Amplify library
+import { API } from 'aws-amplify'
+
+// import query definition
+import { listPosts } from './graphql/queries'
+
+export default function App() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listPosts });
+      setPosts(postData.data.listPosts.items)
+    } catch (err) {
+      console.log({ err })
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+    <div>
+      <h1>Postagram</h1>
+      {
+        posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.name}</h3>
+            <p>{post.description}</p>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/graphql/queries.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      name
+      description
+      image
+      owner
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        image
+        owner
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
